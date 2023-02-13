0f824deed50e6c8e177b0a2f5b7fcdb79f56c340
dr-whale <nekich.cat@gmail.com>
2023-02-13 14:49:44 +0200

    added array responce

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿List<string> ShortStrings (string[] array)
+﻿string[] ShortStrings (string[] array)
 {
-    List<string> shorts = new List<string>();
+    List<string> shortsList = new List<string>();
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length < 4) shorts.Add(array[i]);
+        if (array[i].Length < 4) shortsList.Add(array[i]);
+    }
+    string[] shorts = new string[shortsList.Count];
+    for (int i = 0; i < shorts.Length; i++)
+    {
+        shorts[i] = shortsList[i];
     }
     return shorts;
 }
