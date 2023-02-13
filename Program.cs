0b990755d93e0a9f4f12f0e2464ae5398a98a0a6
dr-whale <nekich.cat@gmail.com>
2023-02-13 14:45:53 +0200

    first version module

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> ShortStrings (string[] array)
+{
+    List<string> shorts = new List<string>();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) shorts.Add(array[i]);
+    }
+    return shorts;
+}
